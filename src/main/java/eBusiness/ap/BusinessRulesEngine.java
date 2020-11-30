9fe99ab44b4393186744d0dbf8ec801e23e73437
Al Plunge <al.plunge@gmail.com>
2020-11-30 19:16:52 +0200

    my first initial commit

---
+++ src/main/java/eBusiness/ap/BusinessRulesEngine.java
@@ -0,0 +1,15 @@
+package eBusiness.ap;
+
+public class BusinessRulesEngine {
+    public void addAction(final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+}
